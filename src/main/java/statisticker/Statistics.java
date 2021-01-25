f14edac7c114c0244402114ac1dc2be63c308acb
chaitrarkanchan <49150199+chaitrarkanchan@users.noreply.github.com>
2021-01-25 16:56:09 +0530

    Update Statistics.java

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 public class Statistics 
 {
+     static class Stats
+    {
+        public Float min=0;
+        public Float max=0;
+        public Float average=0;
+    }
     public static Stats getStatistics(List<___> numbers) {
         //implement the computation of statistics here
     }
