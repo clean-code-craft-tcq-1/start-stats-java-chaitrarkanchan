90ab09c41982c6e25999834ba70778c0990292ab
chaitrarkanchan <49150199+chaitrarkanchan@users.noreply.github.com>
2021-01-28 11:24:41 +0530

    Update IAlerter.java

--- src/main/java/statisticker/alerter/IAlerter.java
+++ src/main/java/statisticker/alerter/IAlerter.java
@@ -2,23 +2,6 @@ package statisticker.alerter;
 
 public interface IAlerter {
 
-	public boolean emailSent;
-	public boolean ledGlows;
-
-	public boolean isEmailSent() {
-		return emailSent;
-	}
-
-	public void setEmailSent(final boolean emailSent) {
-		this.emailSent = emailSent;
-	}
-
-	public boolean isLedGlows() {
-		return ledGlows;
-	}
-
-	public void setLedGlows(final boolean ledGlows) {
-		this.ledGlows = ledGlows;
-	}
+	public void alert();
 
 }
