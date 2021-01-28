5ce0a534f87d78ec8fb69855f1b29c41df249054
chaitrarkanchan <49150199+chaitrarkanchan@users.noreply.github.com>
2021-01-28 11:15:40 +0530

    Update IAlerter.java

--- src/main/java/statisticker/alerter/IAlerter.java
+++ src/main/java/statisticker/alerter/IAlerter.java
@@ -2,6 +2,23 @@ package statisticker.alerter;
 
 public interface IAlerter {
 
-	public void alert();
+	public boolean emailSent;
+	public boolean ledGlows;
+
+	public boolean isEmailSent() {
+		return emailSent;
+	}
+
+	public void setEmailSent(final boolean emailSent) {
+		this.emailSent = emailSent;
+	}
+
+	public boolean isLedGlows() {
+		return ledGlows;
+	}
+
+	public void setLedGlows(final boolean ledGlows) {
+		this.ledGlows = ledGlows;
+	}
 
 }
