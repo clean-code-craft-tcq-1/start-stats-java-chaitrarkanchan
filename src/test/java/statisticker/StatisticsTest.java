2c4720bc768d9b4d6d0435172771c784298838a8
chaitrarkanchan <49150199+chaitrarkanchan@users.noreply.github.com>
2021-01-28 21:18:09 +0530

    Update StatisticsTest.java

--- src/test/java/statisticker/StatisticsTest.java
+++ src/test/java/statisticker/StatisticsTest.java
@@ -53,7 +53,7 @@ public class StatisticsTest
 
         Float[] numbers = {11.5f, 6.9f, 7.5f, 6.6f};
         List<Float> numberList = Arrays.asList(numbers);
-        checker.checkAndAlert(numberList);
+        checker.checkAndAlert(numbers);
         
         assertTrue(emailAlerter.emailSent);
         assertTrue(ledAlerter.ledGlows);
